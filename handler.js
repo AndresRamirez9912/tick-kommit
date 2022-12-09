17321cb9451495c11f8d4bed46c3edeea34707d5
Andres Ramirez <85534471+AndresRamirez9912@users.noreply.github.com>
2022-12-08 23:26:38 -0500

    Add lambda logyc

--- handler.js
+++ handler.js
@@ -1,15 +1,32 @@
 "use strict";
+const AWS = require('aws-sdk');
+const dynamodb = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
+module.exports.tick = async (event, context) => {
+  const tableName = getTableName(event);
+
+  const newEntry = {
+      id: context.awsRequestId,
+      data: event.body,
+      createdAt: new Date().toISOString(),
   };
+
+  try {
+      await dynamodb.put({
+          TableName: tableName,
+          Item: newEntry
+      }).promise();
+  
+      return {
+          statusCode: 201,
+          body: JSON.stringify(newEntry),
+      };
+  } catch (error) {
+      return { error };
+  }
 };
+
+const getTableName = (event) => {
+    if (event.requestContext.stage === "prod") return "tick-prod";
+    return "tick-dev";
+}
